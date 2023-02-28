bee4f4667b734b8c934c6f44baf5f1b2f908062d
Menglong <1074605344@qq.com>
2023-02-28 16:38:28 +0800

    修复idn bug

--- dns-resolution/src/main/java/com/dns/resolution/utils/IDNUtils.java
+++ dns-resolution/src/main/java/com/dns/resolution/utils/IDNUtils.java
@@ -10,25 +10,29 @@ public class IDNUtils {
     private IDNA idna = IDNA.getUTS46Instance(IDNA.DEFAULT);
 
     public String toASCII(String content) {
-        IDNA.Info info = new IDNA.Info();
-        if (content.contains("@")) {
-            if (content.contentEquals("@")) {
-                return "@";
+        if (StringUtils.isEmpty(content)) {
+            return null;
+        } else {
+            IDNA.Info info = new IDNA.Info();
+            if (content.contains("@")) {
+                if (content.contentEquals("@")) {
+                    return "@";
+                } else {
+                    String[] contentSection = StringUtils.splitByWholeSeparator(content, "@");
+                    if (contentSection.length != 2) {
+                        return null;
+                    } else {
+                        return idna.nameToASCII(contentSection[0], new StringBuilder(), info).toString().toLowerCase() + "@" + idna.nameToASCII(contentSection[1], new StringBuilder(), info).toString().toLowerCase();
+                    }
+                }
             } else {
-                String[] contentSection = StringUtils.splitByWholeSeparator(content, "@");
-                if (contentSection.length != 2) {
+                StringBuilder contentBuilder = new StringBuilder();
+                idna.nameToASCII(content, contentBuilder, info);
+                if (info.hasErrors()) {
                     return null;
                 } else {
-                    return idna.nameToASCII(contentSection[0], new StringBuilder(), info).toString().toLowerCase() + "@" + idna.nameToASCII(contentSection[1], new StringBuilder(), info).toString().toLowerCase();
+                    return contentBuilder.toString().toLowerCase();
                 }
-            }
-        } else {
-            StringBuilder contentBuilder = new StringBuilder();
-            idna.nameToASCII(content, contentBuilder, info);
-            if (info.hasErrors()) {
-                return null;
-            } else {
-                return contentBuilder.toString().toLowerCase();
             }
         }
     }
